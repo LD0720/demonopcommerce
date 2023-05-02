c853dd79b6f586e5d55ea16137a0802be7b78565
LD0720 <lobna.work.7@gmail.com>
2023-05-02 18:15:02 +0300

    Initial commit

---
+++ src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -0,0 +1,10 @@
+package org.example.stepDefs;
+import io.cucumber.java.en.Given;
+
+public class D01_registerStepDef {
+    @Given("user go to register page")
+    public void user_go_to_register_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
