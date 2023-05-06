a0200ad2de6aae51976d9143361b180366985819
LD0720 <lobna.work.7@gmail.com>
2023-05-06 12:39:23 +0300

    last update on hovering feature 6/5 12:40

--- src/test/java/org/example/pages/P05_hover.java
+++ src/test/java/org/example/pages/P05_hover.java
@@ -4,14 +4,17 @@ import org.example.stepDefs.Hooks;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class P05_hover {
     public List<WebElement> getCategoriesList(){
-        return Hooks.driver.findElements(By.xpath("//ul[@class='top-menu notmobile']//"));
+        return Hooks.driver.findElements(By.xpath("//ul[@class=\"top-menu notmobile\"]/li"));
+
     }
-    public List<WebElement> getSubCategoryList(){
-        return Hooks.driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//li[1]//ul"));
+    public List<WebElement> getSubCategoryList(String l){
+        return Hooks.driver.findElements(By.xpath(l));
     }
-    public WebElement getTitle(){
+    public WebElement getTitle() {
         return Hooks.driver.findElement(By.className("page-title"));
     }
 }
--- src/test/java/org/example/stepDefs/D05_hoverCategoriesStepDef.java
+++ src/test/java/org/example/stepDefs/D05_hoverCategoriesStepDef.java
@@ -5,19 +5,58 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.example.pages.P05_hover;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class D05_hoverCategoriesStepDef {
     P05_hover hover = new P05_hover();
+    String SelectedCategoryName ;
+    int SelectedCategoryIndex ;
+    List<WebElement> mainLinks;
+    int max;
+    int min;
     @When("hovering over a category")
-    public void HoverOverCategory(){
+    public void hoverCategory() throws InterruptedException {
+        Actions actions=new Actions(Hooks.driver);
+        mainLinks =hover.getCategoriesList();
+        int count = mainLinks.size();
 
+        min = 0;
+         max = count-1;
+        SelectedCategoryIndex = (int)Math.floor(Math.random()*(max-min+1)+min);
+        actions.moveToElement(mainLinks.get(SelectedCategoryIndex)).perform();
+        SelectedCategoryName = mainLinks.get(SelectedCategoryIndex).getText();
+        Thread.sleep(2000);
     }
 
     @And("choosing a sub category")
     public void choosingASubCategory() {
+        SelectedCategoryIndex += 1;
+        String locator = "(//ul[@class='top-menu notmobile']//ul)["+SelectedCategoryIndex+"]/li";
+        List<WebElement> subCategoryLinks = hover.getSubCategoryList(locator);
+        Hooks.driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        String selectedSubCategoryText;
+        if(!subCategoryLinks.isEmpty()) {
+            int subCategoryCount = subCategoryLinks.size();
+            int selectedSubCategory = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            selectedSubCategoryText = subCategoryLinks.get(selectedSubCategory).getText();
+            subCategoryLinks.get(selectedSubCategory).click();
+        }
+        else
+        {
+            mainLinks.get(SelectedCategoryIndex).click();
+        }
+        Hooks.driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
     }
 
     @Then("the sub category title should be relevant to the category")
     public void theSubCategoryTitleShouldBeRelevantToTheCategory() {
+        SoftAssert softAssert = new SoftAssert();
+        String title = hover.getTitle().getText().toLowerCase().trim();
+        softAssert.assertTrue(SelectedCategoryName.toLowerCase().trim().contains(title));
+        softAssert.assertAll();
     }
 }
